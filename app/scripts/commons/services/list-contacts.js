68d140becdd331a8f228e2906cf5476e3052c7c3
VarshaYadav-coder <yadav_varsha91@yahoo.com>
2017-01-09 09:25:59 +0530

    Update list-contacts.js

--- app/scripts/commons/services/list-contacts.js
+++ app/scripts/commons/services/list-contacts.js
@@ -8,8 +8,8 @@
    */
   angular.module('angularContactsListApp')
     .value('listContacts', [
-      {_id: 1, name: 'Niranjan verma', address: 'gurgaon', phone: '91919191919'},
-      {_id: 2, name: 'varsha yadav', address: 'gurgaon, Nº 102', phone: '91919191919'},
+      {_id: 1, name: 'Varsha Yadav', address: 'gurgaon', phone: '91919191919'},
+      {_id: 2, name: 'Deepak Yadav', address: 'gurugram, Nº 102', phone: '91919191919'},
       {_id: 3, name: 'Amit singh ', address: 'gurgaon', phone: '91919191919'},
       {_id: 4, name: 'Jack singh', address: 'gurgaon', phone: '91919191919'},
       {_id: 5, name: 'Joseph  sharma', address: 'gurgaon', phone: '91919191919'},
