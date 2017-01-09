06a804bfa337e1560ee35400733cea135044c185
VarshaYadav-coder <yadav_varsha91@yahoo.com>
2017-01-09 09:45:57 +0530

    Update list-contacts.js

--- app/scripts/commons/services/list-contacts.js
+++ app/scripts/commons/services/list-contacts.js
@@ -8,12 +8,12 @@
    */
   angular.module('angularContactsListApp')
     .value('listContacts', [
-      {_id: 1, name: 'Varsha Yadav', address: 'gurgaon', phone: '91919191919'},
-      {_id: 2, name: 'Deepak Yadav', address: 'gurugram, Nº 102', phone: '91919191919'},
-      {_id: 3, name: 'Amit singh ', address: 'gurgaon', phone: '91919191919'},
-      {_id: 4, name: 'Jack singh', address: 'gurgaon', phone: '91919191919'},
-      {_id: 5, name: 'Joseph  sharma', address: 'gurgaon', phone: '91919191919'},
-      {_id: 6, name: 'Joshua kumar', address: 'gurgaon', phone: '91919191919'}
+      {_id: 1, name: 'Varsha Yadav', department: 'gurgaon', phone: '91919191919', title: 'SSE'},
+      {_id: 2, name: 'Deepak Yadav', department: 'gurugram, Nº 102', phone: '91919191919', title: 'SSE'},
+      {_id: 3, name: 'Amit singh ', department: 'gurgaon', phone: '91919191919', title: 'SSE'},
+      {_id: 4, name: 'Jack singh', department: 'gurgaon', phone: '91919191919', title: 'SSE'},
+      {_id: 5, name: 'Joseph  sharma', department: 'gurgaon', phone: '91919191919', title: 'SSE'},
+      {_id: 6, name: 'Joshua kumar', department: 'gurgaon', phone: '91919191919', title: 'SSE'}
     ]);
 
 }());
